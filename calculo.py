c45a91ec0b1665780fa011d33c4fc74fff17d59a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-15 16:38:58 -0300

    feat: tipagem e operacoes matematicas

---
+++ calculo.py
@@ -0,0 +1,27 @@
+#calculos 
+
+import os
+os.system ("cls")
+
+n1  = float(input("Digite o primeiro numero: "))
+n2  = float(input("Digite o segundo numero: "))
+
+#soma 
+soma = n1 + n2
+
+#Subtração
+sub = n1 - n2
+
+#multiplicação
+mult = n1 * n2
+
+#divisão
+div = n1 / n2
+
+print = (f"primeiro numero: {n1}")
+print = (f"segundo numero: {n2}")
+print = (f"soma: {soma}")
+print = (f"subtração: {sub}")
+print = (f"multiplicação: {mult}")
+print = (f"divisão: {div}")
+
---
+++ tipagem.py
@@ -0,0 +1,13 @@
+#tipagem dinãmica
+
+#limpar o terminal
+import os
+os.system ("cls")
+
+nome = input ("Digite seu nome: ")
+idade = input ("Digite sua idade: ")
+altura = input ("Digite sua altura: ")
+
+print (f"nome: {nome}")
+print(f"idade: {idade}")
+print (f"altura: {altura}")
