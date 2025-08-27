d8a61b5ffa96a1e84923c047dc46420c863b850a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-27 14:26:36 -0300

    fix: modifiquei o código para mostrar se os numeros eram iguais ou nao.

--- atividade_04.py
+++ atividade_04.py
@@ -3,9 +3,12 @@
 import os
 os.system ("cls")
 
-n1  = float(input("Digite o primeiro numero: "))
-n2  = float(input("Digite o segundo numero: "))
 
+# pedindos dados
+n1  = int(input("Digite o primeiro numero: "))
+n2  = int(input("Digite o segundo numero: "))
+
+# operações matematicas
 #soma 
 soma = n1 + n2
 
@@ -15,33 +18,19 @@ sub = n1 - n2
 #multiplicação
 mult = n1 * n2
 
-#divisão
-div = n1 / n2
-
 media = ( n1 + n2) / 2
 
 maior = max(n1, n2)
-
 menor = min(n1, n2)
 
-
-
-
-
-    
-#if n1 > n2:
- #  maior  =  n1
-  # menor = n2
-#else:
-   #maior = n2
-   #menor = n1
-
-
+#Exibindo dados:
 print (f"primeiro numero: {n1}")
 print (f"segundo numero: {n2}")
 print (f"média: {media}")
 print (f"soma: {soma}")
 print (f"produto: {mult}")
-print (f"maior valor: {maior}")
-print (f"menor valor: {menor}")
-print ({resultado})
+if n1 == n2:
+   print("os numeros são iguais!!")
+else:
+   print (f"maior valor: {maior}")
+   print (f"menor valor: {menor}")
