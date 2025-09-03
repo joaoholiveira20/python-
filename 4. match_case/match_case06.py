8c6ca9dc72063dca28746f422f56ea6f285c33a3
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-09-03 16:37:07 -0300

    feat: corrigindo para ficar mais bonito

--- 4. match_case/match_case06.py
+++ 4. match_case/match_case06.py
@@ -9,9 +9,9 @@ altura = float(input("Qual a sua altura: "))
 match genero:
     case "m":
         resultado_m= (72.7 * altura) - 58
-        print(f"Seu peso ideal é: {resultado_m:2.2f}")
+        print(f"Seu peso ideal é: Kg {resultado_m:2.2f}")
     case "f":
         resultado_f = (62.1 * altura) - 44.7
-        print(f"Seu peso ideal é: Kg:{resultado_f:2.2f}")
+        print(f"Seu peso ideal é: Kg {resultado_f:2.2f}")
     case _:
         print("opção invalida")
